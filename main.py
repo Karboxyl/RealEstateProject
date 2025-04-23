03e4a697a518ec6aa0e8f0272fc4e283a43f1b15
unknown <milan.haviar1@gmail.com>
2025-04-23 11:54:48 +0200

    Connected to Nehnutelnosti

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import numpy as np
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import json
 
-msg = "Roll a dice!"
-print(msg)
-msg="exit"
-print(msg)
+
+def main():
+    url="https://www.nehnutelnosti.sk/vysledky/bernolakovo?page=2"
+    response=requests.get(url)
+    print(response)
+
+    soup=BeautifulSoup(response.text,'html.parser')
+    post_name = soup.find_all("h2", attrs={"data-test-id": "text"})
+    post_price=soup.find_all(attrs={"class":"MuiTypography-root MuiTypography-h5 mui-7e5awq","data-test-id": "text"})
+    post_location=soup.find_all("p",attrs={"class":"MuiTypography-root MuiTypography-body3 MuiTypography-noWrap mui-e9ka76","data-test-id": "text"})
 
 
-print(np.random.randint(1,9))
+
+
+    for posting in post_location:
+        print(posting)
+        print(posting.text.strip())
+
+main()
